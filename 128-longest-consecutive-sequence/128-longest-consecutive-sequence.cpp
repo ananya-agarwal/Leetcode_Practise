ff8b027955f7bf3c644f15f4001b28710afb5071
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-07-11 16:00:01 +0530

    Time: 2384 ms (5.01%), Space: 51.2 MB (25.35%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,24 +2,48 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums)
     {
-        int n=nums.size(),sg=1,sl=1;
-        if(n==0)
-            return 0;
-        
-        sort(nums.begin(), nums.end());
-        nums.erase( unique(nums.begin(), nums.end() ), nums.end() );
-        
-        for(int i=0;i<n-1;i++)
+      set < int > hashSet;
+      for (int num: nums)
+        hashSet.insert(num);
+
+      int longestStreak = 0;
+
+      for (int num: nums) {
+        if (!hashSet.count(num - 1))
         {
-            if(nums[i]+1 == nums[i+1])
-            {
-                sl++;
-                if(sl>sg)
-                    sg=sl;
-            }
-            else
-                sl=1;
+          int currentNum = num;
+          int currentStreak = 1;
+
+          while (hashSet.count(currentNum + 1))
+          {
+            currentNum += 1;
+            currentStreak += 1;
+          }
+
+          longestStreak = max(longestStreak, currentStreak);
         }
-        return sg;
     }
+    return longestStreak;
+}
 };
+
+
+//int n=nums.size(),sg=1,sl=1;
+//         if(n==0)
+//             return 0;
+        
+//         sort(nums.begin(), nums.end());
+//         nums.erase( unique(nums.begin(), nums.end() ), nums.end() );
+        
+//         for(int i=0;i<n-1;i++)
+//         {
+//             if(nums[i]+1 == nums[i+1])
+//             {
+//                 sl++;
+//                 if(sl>sg)
+//                     sg=sl;
+//             }
+//             else
+//                 sl=1;
+//         }
+//         return sg;
