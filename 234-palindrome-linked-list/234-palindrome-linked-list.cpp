1c13ee83b77c673f4cf881426cc28cb0c5bf1841
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-07-15 13:52:53 +0530

    Time: 366 ms (47.27%), Space: 118.1 MB (48.51%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,49 +11,50 @@
 class Solution {
 public:
    
-    ListNode* reverse(ListNode* ptr)
+    ListNode* reverse(ListNode* h1)
     {
-    ListNode* pre=NULL;
-    ListNode* nex=NULL;
-    while(ptr!=NULL) 
-    {
-        nex = ptr->next;
-        ptr->next = pre;
-        pre=ptr;
-        ptr=nex;
+        ListNode* pre=NULL;
+        ListNode* nex=NULL;
+        
+        while(h1!=NULL) 
+        {
+            nex = h1->next;
+            h1->next = pre;
+            pre=h1;
+            h1=nex;
+        }
+        return pre;
     }
-    return pre;
-}
 
     
-bool isPalindrome(ListNode* head) 
-{
-    if(head==NULL||head->next==NULL) 
+    bool isPalindrome(ListNode* head) 
+    {
+        if(head==NULL||head->next==NULL) 
+            return true;
+
+         ListNode* slow = head;
+         ListNode* fast = head;
+
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        slow->next = reverse(slow->next);
+        slow = slow->next;
+        ListNode* dummy = head;
+
+        while(slow!=NULL)
+        {
+            if(dummy->val != slow->val)
+                return false;
+
+            dummy = dummy->next;
+            slow = slow->next;
+        }
         return true;
-        
-     ListNode* slow = head;
-     ListNode* fast = head;
-        
-    while(fast->next!=NULL && fast->next->next!=NULL)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
     }
-        
-    slow->next = reverse(slow->next);
-    slow = slow->next;
-    ListNode* dummy = head;
-        
-    while(slow!=NULL)
-    {
-        if(dummy->val != slow->val)
-            return false;
-        
-        dummy = dummy->next;
-        slow = slow->next;
-    }
-    return true;
-}
 };
 //method1: saari value ko array me daaldo and then loop lagake array ke half tak chck krlo for palindrome
 //method2: phle half tak loop chalao and mid vale pr slow pointr rkdo then second hlf ko reverse krdo. strt hlf pr ab loop lagake
