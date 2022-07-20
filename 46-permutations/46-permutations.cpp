174fb989526d5399b02657ca2129770a3bd3593d
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-07-20 12:01:34 +0530

    Time: 4 ms (65.77%), Space: 7.9 MB (45.31%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,36 +1,62 @@
 class Solution {
 public:
     
-    //method1: recursion
-    
-    void helper(vector<int> &nums, vector<int> &freq, vector<vector<int>> &ans, vector<int> &ds)
+    vector<vector< int>> result;
+    // Backtracking
+    void permutation(vector<int> &nums,int i)
     {
-        if(ds.size() == nums.size())
-        {
-            ans.push_back(ds);
-            return;
-        }
+        int n=nums.size();
         
-        for(int i=0;i<nums.size();i++)
+        if(i==n-1)
+            result.push_back(nums);
+
+        for(int j=i;j<n;j++)
         {
-            if(!freq[i])
-            {
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                helper(nums, freq, ans, ds);
-                freq[i]=0;
-                ds.pop_back();
-            }
+            swap( nums[i],nums[j]);
+            permutation(nums,i+1);
+            swap( nums[i],nums[j]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums)
     {
-        int n=nums.size();
-        vector<int> freq(n,0);
-        vector<vector<int>> ans;
-        vector<int> ds;
-        helper(nums, freq, ans, ds);
-        return ans;
+        permutation(nums,0);
+        return result;
     }
 };
+
+
+
+//method1: recursion
+
+ 
+//     void helper(vector<int> &nums, vector<int> &freq, vector<vector<int>> &ans, vector<int> &ds)
+//     {
+//         if(ds.size() == nums.size())
+//         {
+//             ans.push_back(ds);
+//             return;
+//         }
+
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(!freq[i])
+//             {
+//                 ds.push_back(nums[i]);
+//                 freq[i]=1;
+//                 helper(nums, freq, ans, ds);
+//                 freq[i]=0;
+//                 ds.pop_back();
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums)
+//     {
+//         int n=nums.size();
+//         vector<int> freq(n,0);
+//         vector<vector<int>> ans;
+//         vector<int> ds;
+//         helper(nums, freq, ans, ds);
+//         return ans;
+//     }
