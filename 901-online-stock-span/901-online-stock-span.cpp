d1a2c6282cde92a4df4e4e0189b6c098b7bc5759
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-07-27 22:12:23 +0530

    Time: 506 ms (6.18%), Space: 84.3 MB (63.04%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.cpp
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -19,10 +19,11 @@ public:
             st.pop();
         }
         st.push({price, c});
-        
         return c;
     }
     
+    //is ques me we assume ki apne aap bar bar price chnge ho rha ha har iteration 
+   // me and we r returning ans for each proce value and asn is getting saved
    // uss element ke left pr jaaek dekhe ki current elemnt se bada elemnt kab aaya tha and ans for that elemnt will be diff of indices of both
 };
 
