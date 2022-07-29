ac5ad48bf0cc3a3885c69a6ed653a4450f6d7ea1
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-07-29 20:20:11 +0530

    Time: 3 ms (94.20%), Space: 7.7 MB (56.49%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -0,0 +1,64 @@
+// #include<stack>
+
+// class Solution
+// {
+// public:
+//     string reverseWords(string s) 
+//     {
+//         s+=" ";
+//         string temp="", ans="";
+//         stack<string> st;
+//         int n=s.size();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(s[i] == ' ')
+//             {
+//                 st.push(temp);
+//                 temp="";
+//             }
+//             else
+//                 temp += s[i];
+//         }
+
+//         while(!st.empty())
+//         {
+//             string c=st.top();
+//             ans = ans+c+" ";
+//             st.pop();
+//         }
+        
+//         ans.pop_back();
+//         return ans;
+//     }
+// };
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string>st;
+        string str = "";
+        for(int i = 0;i<s.length();i++){
+            if(s[i] != ' ' ){
+                str += s[i];
+                continue;
+            }
+            else{
+                if(str!="")
+                   st.push(str);
+                str="";
+            }
+        }
+        if(str!=""){
+            st.push(str);
+        }
+        string ans ="";
+        while(!st.empty()){
+            ans += st.top() + " ";
+            st.pop();
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
