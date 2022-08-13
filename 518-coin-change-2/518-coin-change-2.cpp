e64866c0af528010b18e9632327cb68cb7ed3cd2
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-08-13 17:57:54 +0530

    Time: 7 ms (98.44%), Space: 7 MB (94.94%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,31 +1,14 @@
 class Solution {
 public:
-    int change(int T, vector<int>& arr) 
+    
+    int change(int amount, vector<int>& coins)
     {
-        int n=arr.size();
-        vector<vector<long>> dp(n,vector<long>(T+1,0));
-        
-        //Initializing base condition
-        for(int i=0;i<=T;i++)
-        {
-            if(i%arr[0]==0)
-                dp[0][i]=1;
-            // Else condition is automatically fulfilled,
-            // as dp array is initialized to zero
-        }
+        vector<int> dp(amount+1);
+        dp[0]=1;
 
-        for(int ind=1; ind<n;ind++)
-        {
-            for(int target=0;target<=T;target++)
-            {
-                long notTaken = dp[ind-1][target];
-                long taken = 0;
-                if(arr[ind]<=target)
-                    taken = dp[ind][target-arr[ind]];
-                dp[ind][target] = notTaken + taken;
-            }
-        }
+        for(int coin:coins)
+            for(int i=coin;i<=amount;i++) dp[i]+=dp[i-coin];
 
-        return dp[n-1][T];
+        return dp[amount];
     }
 };
