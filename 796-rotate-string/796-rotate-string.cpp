dad834cc017fa9bb8e4232aa82b3a50c7ea59c91
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-08-16 16:15:26 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (25.51%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -8,13 +8,11 @@ public:
     {
         int n = s.size();
         
-        // if(n != goal.size()) 
-        //     return false;
-        
         for(int i = 0;i<n;i++)
         {
             if(s.substr(i,n)==goal) 
                 return true;
+            
             s+=s[i];
         }
         return false;
