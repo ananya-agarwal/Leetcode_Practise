1a8c278fd1941c81dcc242e6c6646a56c86ac29d
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-08-16 18:14:04 +0530

    Time: 10 ms (20.67%), Space: 8.5 MB (61.19%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -12,9 +12,13 @@ public:
 		for(string i : words)
         {
 			string s = "";
-			for(char c : i)
-				s += code[c - 'a'];
+			for(char rt : i)
+				s += code[rt - 97];
+            //code ke andar ab c-a say 2 aarha ha that is c ke liye 2nd index vale se elemnts uthao
 			
+            //agar koi abhi bhi first time aarha ha toh we will not insert it into hashmap
+            //and use count nahi krnege
+            
 			if(mp[s]++ == 0) 
 				ans++;
 		}
