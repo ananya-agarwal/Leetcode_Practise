4ec97a85d6f161366fd19ea0c8f882226f15931e
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-08-19 13:19:11 +0530

    Time: 8 ms (95.10%), Space: 9.1 MB (60.84%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -5,43 +5,38 @@ public:
     
         int dp[n+1][sum+1];
 
-    	for(int i=0; i<=n; i++) dp[i][0] = 1;
-    	for(int j=1; j<=sum; j++) dp[0][j] = 0;
-    
+    	for(int i=0; i<=n; i++) 
+            dp[i][0] = 1;
+    	for(int j=1; j<=sum; j++)
+            dp[0][j] = 0;
     
     	for(int i=1; i<=n; i++)
     	{
     		for(int j=0; j<=sum; j++)
     		{
-    		    if(arr[i-1]<=j){
+    		    if(arr[i-1]<=j)
     		        dp[i][j]=dp[i-1][j-arr[i-1]] + dp[i-1][j];
-    		    }
-    		    else{
+    		    else
     		        dp[i][j]=dp[i-1][j];
-    		    }
     		}	
     	}
-
 	    return dp[n][sum];
     }
         
-    int findTargetSumWays(vector<int>& arr, int target) {
+    int findTargetSumWays(vector<int>& arr, int target)
+    {
         target=abs(target);
         int n=arr.size();
         int sum=0;
         for(int i=0;i<n;i++)
-        {
             sum+=arr[i];
-        }
-        
+            
         if(sum<target||(sum+target)%2!=0)
             return 0;
+        
         int val=(target+sum)/2;
-        
-        return perfectSum(arr,n,val);
-        
+        return perfectSum(arr,n,val); 
     }
-    
 };
 
 
