98e456d08c0474a56f7cf42b00819db24a9f232e
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-08-19 08:22:23 +0530

    Time: 330 ms (33.64%), Space: 77.2 MB (29.51%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) 
     {
-        unordered_map<int,int>hash;
-
+        unordered_map<int,int>mp;
+        
         for(int i=0;i<nums.size();i++)
         {
-           if(hash.count(nums[i])) // when the key is already existed (duplicate) in the hashmap                                                  // then check the condition
-                if(i-hash[nums[i]]<=k) 
+           if(mp.count(nums[i])) // when the key is already existed (duplicate) in the hashmap                                                  // then check the condition
+                if(i-mp[nums[i]]<=k) 
                     return true;
             
-            hash[nums[i]]= i; // nums[i] is key and index number is value 
+            mp[nums[i]]= i; // nums[i] is key and index number is value 
         }
         return false;
     }
