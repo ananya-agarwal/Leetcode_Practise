ad5c54ac1f140cfcd41368acbb6e5f9f32681472
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-08-21 14:50:17 +0530

    Time: 2763 ms (5.01%), Space: 9 MB (77.00%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
-    int perfectSum(vector<int>& arr, int n, int sum)
-	{
-    
-        int dp[n+1][sum+1];
-
-    	for(int i=0; i<=n; i++) 
-            dp[i][0] = 1;
-        
-    	for(int j=1; j<=sum; j++)
-            dp[0][j] = 0;
-    
-    	for(int i=1; i<=n; i++)
-    	{
-    		for(int j=0; j<=sum; j++)
-    		{
-    		    if(arr[i-1]<=j)
-    		        dp[i][j]=dp[i-1][j-arr[i-1]] + dp[i-1][j];
-    		    else
-    		        dp[i][j]=dp[i-1][j];
-    		}	
-    	}
-	    return dp[n][sum];
+    void solve(int &c, int sum, int i, vector<int>& nums, int target){
+        int n = nums.size();
+        if(i >= n) {
+            if(sum == target) c++;
+            return;
+        }    
+        solve(c, sum-nums[i], i+1, nums, target);
+        solve(c, sum+nums[i], i+1, nums, target);
     }
-        
-    int findTargetSumWays(vector<int>& arr, int target)
-    {
-        target=abs(target);
-        int n=arr.size(),sum=0;
-        
-        for(int i=0;i<n;i++)
-            sum+=arr[i];
-            
-        if(sum<target||(sum+target)%2!=0)
-            return 0;
-        
-        int val=(target+sum)/2;
-        return perfectSum(arr,n,val); 
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int c = 0;
+        solve(c, 0, 0, nums, target);
+        return c;
     }
 };
 
