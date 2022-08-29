b4fc7ffad8e97839aa218e01c1c5d7c9e150d442
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2022-08-29 10:38:10 +0530

    Time: 296 ms (5.25%), Space: 83.5 MB (48.71%) - LeetHub

--- 860-lemonade-change/860-lemonade-change.cpp
+++ 860-lemonade-change/860-lemonade-change.cpp
@@ -2,45 +2,73 @@ class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) 
     {
-        unordered_map<int, int> m;
-        int change = 0;
         
-        for(int i = 0 ; i < bills.size(); i++)
-        {
-            m[bills[i]]++;
+        
+//         unordered_map<int, int> m;
+//         int change = 0;
+        
+//         for(int i = 0 ; i < bills.size(); i++)
+//         {
+//             m[bills[i]]++;
             
-            //sara kaam tabhi krna ha jab coin 5 ka na ho
-            if(bills[i] > 5)
-            {
-                change = bills[i] - 5;
+//             //sara kaam tabhi krna ha jab coin 5 ka na ho
+//             if(bills[i] > 5)
+//             {
+//                 change = bills[i] - 5;
                 
-                if(change == 5)
-                {
-                    if(m[5] > 0)
-                        m[5]--;
-                    else
-                        return false;
-                }
-                
-                
-                //change = 10
-                else
-                {
-                    if(m[10] > 0 and m[5] > 0)
-                    {
-                        m[10]--;
-                        m[5]--;
-                    }
-                    else if(m[5] >= 3)
-                        m[5] -= 3;
-                    else
-                        return false;
-                }
+//                 if(change == 5)
+//                 {
+//                     if(m[5] > 0)
+//                         m[5]--;
+//                     else
+//                         return false;
+//                 }
                 
                 
-            }
-        }
+//                 //change = 15
+//                 else
+//                 {
+//                     if(m[10] > 0 and m[5] > 0)
+//                     {
+//                         m[10]--;
+//                         m[5]--;
+//                     }
+//                     else if(m[5] >= 3)
+//                         m[5] -= 3;
+//                     else
+//                         return false;
+//                 }
+                
+                
+//             }
+//         }
         
+//         return true;
+        
+        
+        int five=0, ten=0;
+        for(auto i:bills)
+        {
+            if(i==5) 
+                five++;
+            
+            else if(i==10) 
+            {
+                five--;
+                ten++;
+            }
+            
+            else if(ten>0)
+            {
+                ten--; 
+                five--;
+            }
+            
+            else
+                five -=3;
+            
+            if(five<0) return false;
+        }
         return true;
     }
 };
